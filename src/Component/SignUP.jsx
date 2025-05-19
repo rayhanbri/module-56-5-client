4ca529fc06ff37c3e9c9ec5b6887dd30e7dd3fae
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-19 13:30:22 -0700

    user create with email,pass

--- src/Component/SignUP.jsx
+++ src/Component/SignUP.jsx
@@ -4,9 +4,32 @@ import { AuthContext } from '../Context/AuthContext';
 const SignUP = () => {
     // ekto test korchi kaj korche ki na 
 
-    const {user}= use(AuthContext)
+    const {createUser}= use(AuthContext)
 
-    console.log(user)
+    console.log(createUser)
+  
+
+    const handleSignUP = e => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form)
+        const email = formData.get('email')
+        const password = formData.get('password')
+        // console.log(email,password)
+
+        // create  user in firebase 
+        createUser(email,password)
+        .then(result => {
+            console.log(result.user)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+
+    }
+
+
     return (
        <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,11 +37,11 @@ const SignUP = () => {
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-4xl font-bold">Signup now!</h1>
-        <form className="fieldset">
+        <form onSubmit={handleSignUP} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Login</button>
         </form>
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,17 +1,30 @@
-import React, { Children } from 'react';
+
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase-init';
 
 
 const AuthProvider = ({ children }) => {
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+
+
     // ekto check kore nei kaj krockhe ki na 
     const userInfo = {
-        user:'rayhan bhai'
+        createUser
     }
+    
     return (
-        <AuthContext value={userInfo} >
+        <AuthContext value={userInfo}>
             {children}
         </AuthContext>
     );
 };
+
+
 
 export default AuthProvider;
